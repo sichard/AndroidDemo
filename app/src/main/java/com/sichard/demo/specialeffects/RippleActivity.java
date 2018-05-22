ddf31db5e1dfd3e7188a532fcb973d182a0f4cbe
sichardcao <sichardcao@tcl.com>
2018-05-22 14:55:13 +0800

    加入ripple特效view

---
+++ app/src/main/java/com/sichard/demo/specialeffects/RippleActivity.java
@@ -0,0 +1,22 @@
+package com.sichard.demo.specialeffects;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.android.sichard.common.BaseActivity;
+import com.sichard.demo.R;
+
+/**
+ * <br>类描述:涟漪效果的
+ * <br>详细描述:
+ * <br><b>Author sichard</b>
+ * <br><b>Date 18-5-22</b>
+ */
+public class RippleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ripple);
+    }
+}
